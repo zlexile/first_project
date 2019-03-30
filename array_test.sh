8ef679ed574ad59737e27163399bda1a9f5f1035
zlexile <48973733+zlexile@users.noreply.github.com>
2019-03-30 23:11:09 +0900

    Update array_test.sh

--- array_test.sh
+++ array_test.sh
@@ -4,19 +4,20 @@ declare -a word
 i=0
 while [ true ]
 do
-read -p "Enter a new word: " word[i]
-if [ "${word[$i]}" = "quit" ] ; then
-exit 0
-elif [ "${word[$i]}" = "list" ] ; then
-let i=i-1
-for list_all in $(seq 0 $i)
-do
-echo -n "${word[$list_all]} "
-done
-echo
-else
-echo "Length: ${#word[@]}"
-let i=i+1
-fi
+    read -p "Enter a new word: " word[i]
+  if [ "${word[$i]}" = "quit" ] ; then
+    exit 0
+  elif [ "${word[$i]}" = "list" ] ; then
+    let i=i-1
+    for list_all in $(seq 0 $i)
+    do
+      echo -n "${word[$list_all]} "
+    done
+      echo
+      let i=i+1
+  else
+    echo "Length: ${#word[@]}"
+    let i=i+1
+  fi
 done
 
